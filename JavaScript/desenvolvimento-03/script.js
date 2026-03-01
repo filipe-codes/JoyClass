9594e1214fe3411097715af1e0e89150ff489e97
Filipe <filipe.figr@gmail.com>
2026-03-01 20:47:12 -0300

    desenvolvimento 03 js

---
+++ JavaScript/desenvolvimento-03/script.js
@@ -0,0 +1,9 @@
+let titulo = document.getElementById("titulo");
+let aElement = document.querySelector("a");
+let ulElement = document.querySelector("ul");
+let olElement = document.getElementById("lista-ordenada");
+
+titulo.innerText = "Titulo";
+aElement.innerText = "Site da Prozeducação";
+ulElement.innerHTML = "<li>Item 1</li><li>Item 2</li><li>Item 3</li>";
+olElement.innerHTML = "<li>Item 1</li><li>Item 2</li><li>Item 3</li>";  
